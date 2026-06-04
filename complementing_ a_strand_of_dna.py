482e0b5fded713426528f0808e533a72f3260b37
worried_ab_life <andrewiskandarov2011@gmail.com>
2026-06-04 17:50:14 -0400

    Refactor DNA complementing functions and improve comments

--- complementing_ a_strand_of_dna.py
+++ complementing_ a_strand_of_dna.py
@@ -1,5 +1,5 @@
 def read_dna_from_txt(file_path: str) -> str:
-    # Function that reads nucleotide sequence from txt file
+    # Reads nucleotide sequence from txt file
     sequence = "" # Initialize empty variable for the sequence
     
     with open(file_path) as f: # Open the file for reading
@@ -13,24 +13,24 @@ def read_dna_from_txt(file_path: str) -> str:
 def change_nucleotides(sequence: str) -> str:
     dna = ""  # Initialize empty variable for the DNA
     
-    for nucleotide in sequence: # Creates an elif function that replaces A, T, C, G Nucleotides with complementary bases
-        if nucleotide == 'A': # Changes A with T
+    for nucleotide in sequence: # Replace each nucleotide with its complementary base
+        if nucleotide == 'A':
             dna += 'T'
-        elif nucleotide == 'T': # Changes T with A
+        elif nucleotide == 'T':
             dna += 'A'
-        elif nucleotide == 'C': # Changes C with G
+        elif nucleotide == 'C':
             dna += 'G'
-        elif nucleotide == 'G': # Changes G with C
+        elif nucleotide == 'G':
             dna += 'C'
         else:
-            dna += nucleotide # Protection for the code in case the symbol is not A, T, C, G
+            dna += nucleotide # Protection in case the symbol is not A, T, C, G
 
-    return dna [:: -1] # Function of reversing DNA (slicing)
+    return dna[::-1] # Reverse the complemented strand
 
 
 if __name__ == "__main__": # Entry point of the code
     file = "rosalind_revc.txt" # Assign the filename to the variable
-    seq = read_dna_from_txt(file) # Import the specific variable from the other code
-    dna_final = change_nucleotides(seq) # Import the specific variable from the other code
+    seq = read_dna_from_txt(file) 
+    dna_final = change_nucleotides(seq)
     
     print(dna_final) # Printing the final result
